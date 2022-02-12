bf10d75235636577dbb3196bf90b01c22d9658a9
513sojin <513sojin@naver.com>
2022-02-12 23:31:54 +0900

    Initial Commit

---
+++ Sulove/SuloveApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuloveApp.swift
+//  Sulove
+//
+//  Created by Sojin Lee on 2022/02/12.
+//
+
+import SwiftUI
+
+@main
+struct SuloveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
